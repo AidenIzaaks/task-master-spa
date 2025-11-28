0133cebfd4abfd37d869a79b09acefb96f0674d3
Aiden Izaaks <aizaaks@metro.com.na>
2025-11-28 15:38:31 +0200

    Move frontend files to root for GitHub Pages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
